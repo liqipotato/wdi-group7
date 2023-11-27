f0247fe20c8c74b10b8053eafe1e1d3d27a0fb93
liqipotato <89569278+liqipotato@users.noreply.github.com>
2023-11-27 08:40:04 +0800

    created blocking methods

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/CompanyBlockingKeyByIndustryGenerator.java
@@ -0,0 +1,39 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking;
+
+import java.util.Arrays;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Company;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class CompanyBlockingKeyByIndustryGenerator extends RecordBlockingKeyGenerator <Company, Attribute>{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+	public void generateBlockingKeys(Company company, Processable<Correspondence<Attribute, Matchable>> correspondences,
+			DataIterator<Pair<String, Company>> resultCollector) {
+        
+        String industry = company.getIndustry();
+        String[] tokens = new String[0]; 
+        
+        if (industry != null) {
+            tokens = industry.split(" ");
+        }
+        
+        String blockingKeyValue = "";
+        System.out.println(Arrays.toString(tokens));
+        for (int i = 0; i <= 2 && i < tokens.length; i++) {
+            if (tokens[i] != null) {
+                blockingKeyValue += tokens[i].substring(0, Math.min(2, tokens[i].length())).toUpperCase();
+            }
+        }
+        
+        resultCollector.next(new Pair<>(blockingKeyValue, company));
+	}
+
+}
--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_Main.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import org.slf4j.Logger;
 
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.CompanyBlockingKeyByHqCityGenerator;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.CompanyBlockingKeyByIndustryGenerator;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking.MovieBlockingKeyByTitleGenerator;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.CompanyNameComparatorEqual;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.CompanyNameComparatorJaccard;
@@ -50,20 +52,20 @@ public class IR_Main {
 		HashedDataSet<Company, Attribute> usCompanies = new HashedDataSet<>();
 		new CompanyXMLReader().loadFromXML(new File("data/input/us_companies_translated.xml"), "/companies/company",
 				usCompanies);
-		// HashedDataSet<Company, Attribute> dataFortune500 = new HashedDataSet<>();
-		// new CompanyXMLReader().loadFromXML(new File("data/input/fortune_500_translated.xml"), "/companies/company",
-		// 		dataFortune500);
-		HashedDataSet<Company, Attribute> dataTop2000 = new HashedDataSet<>();
-		new CompanyXMLReader().loadFromXML(new File("data/input/top_2000_translated.xml"), "/companies/company",
-				dataTop2000);
+		HashedDataSet<Company, Attribute> dataFortune500 = new HashedDataSet<>();
+		new CompanyXMLReader().loadFromXML(new File("data/input/fortune_500_translated.xml"), "/companies/company",
+				dataFortune500);
+		// HashedDataSet<Company, Attribute> dataTop2000 = new HashedDataSet<>();
+		// new CompanyXMLReader().loadFromXML(new File("data/input/top_2000_translated.xml"), "/companies/company",
+		// 		dataTop2000);
 
 		// // load the gold standard
 		logger.info("*\tLoading gold standard\t*");
 		MatchingGoldStandard gsTest = new MatchingGoldStandard();
-		// gsTest.loadFromCSVFile(new File(
-		// "data/goldstandard/fortune_500_2_us_companies.csv")); // fortune 500 against us companies
 		gsTest.loadFromCSVFile(new File(
-		"data/goldstandard/top_2000_2_us_companies.csv")); // top 2000 against us companies
+		"data/goldstandard/fortune_500_2_us_companies.csv")); // fortune 500 against us companies
+		// gsTest.loadFromCSVFile(new File(
+		// "data/goldstandard/top_2000_2_us_companies.csv")); // top 2000 against us companies
 
 		// create a matching rule
 		LinearCombinationMatchingRule<Company, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
@@ -76,27 +78,25 @@ public class IR_Main {
 		matchingRule.addComparator(new CompanyNameComparatorLevenshtein(), 1.0);
 
 		// create a blocker (blocking strategy)
-		// StandardRecordBlocker<Movie, Attribute> blocker = new
-		// StandardRecordBlocker<Movie, Attribute>(
-		// new MovieBlockingKeyByTitleGenerator());
-		NoBlocker<Company, Attribute> blocker = new NoBlocker<>();
-		// SortedNeighbourhoodBlocker<Movie, Attribute, Attribute> blocker = new
-		// SortedNeighbourhoodBlocker<>(new MovieBlockingKeyByTitleGenerator(), 1);
-		// blocker.setMeasureBlockSizes(true);
+		// StandardRecordBlocker<Company, Attribute> blocker = new StandardRecordBlocker<Company, Attribute>(new CompanyBlockingKeyByIndustryGenerator());
+		// NoBlocker<Company, Attribute> blocker = new NoBlocker<>();
+		SortedNeighbourhoodBlocker<Company, Attribute, Attribute> blocker = new SortedNeighbourhoodBlocker<>(new CompanyBlockingKeyByIndustryGenerator(), 30);
+		blocker.setMeasureBlockSizes(true);
+		
 		// Write debug results to file:
-		// blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 100);
+		blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 100);
 
 		// Initialize Matching Engine
 		MatchingEngine<Company, Attribute> engine = new MatchingEngine<>();
 
 		// Execute the matching
 		logger.info("*\tRunning identity resolution\t*");
-		// Processable<Correspondence<Company, Attribute>> correspondences = engine.runIdentityResolution(
-		// 		usCompanies, dataFortune500, null, matchingRule,
-		// 		blocker); // fortune 500 against us companies
 		Processable<Correspondence<Company, Attribute>> correspondences = engine.runIdentityResolution(
-				usCompanies, dataTop2000, null, matchingRule,
+				usCompanies, dataFortune500, null, matchingRule,
 				blocker); // fortune 500 against us companies
+		// Processable<Correspondence<Company, Attribute>> correspondences = engine.runIdentityResolution(
+		// 		usCompanies, dataTop2000, null, matchingRule,
+		// 		blocker); // Forbes 2000 against us companies
 
 		// Create a top-1 global matching
 		correspondences = engine.getTopKInstanceCorrespondences(correspondences, 1, 0.0);
@@ -108,8 +108,8 @@ public class IR_Main {
 		// correspondences = maxWeight.getResult();
 
 		// write the correspondences to the output file
-		// new CSVCorrespondenceFormatter().writeCSV(new File("data/output/fortune_500_2_us_companies_correspondences.csv"), correspondences); // fortune 500 against us companies
-		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/top_2000_2_us_companies_correspondences.csv"), correspondences); // top 2000 against us companies
+		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/fortune_500_2_us_companies_correspondences.csv"), correspondences); // fortune 500 against us companies
+		// new CSVCorrespondenceFormatter().writeCSV(new File("data/output/top_2000_2_us_companies_correspondences.csv"), correspondences); // top 2000 against us companies
 	
 		// logger.info("*\tEvaluating result\t*");
 		// // evaluate your result
@@ -118,7 +118,7 @@ public class IR_Main {
 		Performance perfTest = evaluator.evaluateMatching(correspondences, gsTest);
 
 		// // print the evaluation result
-		logger.info("Forbes Top 2000 Companies <-> US Companies");
+		logger.info("Fortune 500 Companies <-> US Companies");
 		logger.info(String.format(
 		"Precision: %.4f", perfTest.getPrecision()));
 		logger.info(String.format(
